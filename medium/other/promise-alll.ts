d8e8d9bc8e9057b1ebc2b568ae90b0e302355569
hiroki katsu <sns3655512@gmail.com>
2022-06-04 00:20:42 +0900

    できなかった問題追加

--- medium/other/promise-alll.ts
+++ medium/other/promise-alll.ts
@@ -3,3 +3,7 @@ const promise2 = 42;
 const promise3 = new Promise<string>((resolve, reject) => {
   setTimeout(resolve, 100, "foo");
 });
+
+const p = Promise.all([promise1, promise2, promise3] as const);
+
+type PromiseALL<T extends Promise<any[]>>
